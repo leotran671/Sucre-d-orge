1c9a436b91afaea82a9a60536f39856e151df78e
Hugo <mollow@Mollow.local>
2022-09-26 15:18:49 +0200

    Dockerize: Fix PMA

--- src/index.php
+++ src/index.php
@@ -1,10 +1,12 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-  <title>Hello</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-  <p>Hello World !</p>
+    toto
 </body>
 </html>
-<?php phpinfo(); ?>
