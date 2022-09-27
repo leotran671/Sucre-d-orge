4055945ca1207b8a8456b4ef7585d3c842977ceb
Hugo <mollow@Mollow.local>
2022-09-27 12:51:18 +0200

    Refacto && Authentication Persistant

---
+++ src/includes/db.php
@@ -0,0 +1,2 @@
+<?php
+$bdd = new PDO('mysql:host=db:3306;dbname=db;charset=utf8;', 'root', 'root');
--- src/includes/nav.php
+++ src/includes/nav.php
@@ -1,5 +1,10 @@
 <nav>
     <a <?= $_SERVER['REQUEST_URI'] == "/" || $_SERVER['REQUEST_URI'] == "/index.php" ? "aria-disabled=true" : "href=/" ?> >Accueil</a>
-    <a <?= $_SERVER['REQUEST_URI'] == "/login.php" ? "aria-disabled=true" : "href=/login.php" ?> >Connexion</a>
-    <a <?= $_SERVER['REQUEST_URI'] == "/signin.php" ? "aria-disabled=true" : "href=/signin.php" ?> >Inscription</a>
+    
+    <?php if (isset($_SESSION['user'])) : ?>
+        <a href="/logout.php">Déconnexion</a>
+    <?php else : ?>
+        <a <?= $_SERVER['REQUEST_URI'] == "/login.php" ? "aria-disabled=true" : "href=/login.php" ?> >Connexion</a>
+        <a <?= $_SERVER['REQUEST_URI'] == "/signin.php" ? "aria-disabled=true" : "href=/signin.php" ?> >Inscription</a>
+    <?php endif ?>
 </nav>
