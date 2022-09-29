320b0bab756d6d2e0b9d61a16adf712b1e4833c5
Raphalalc <raphaelalc19@gmail.com>
2022-09-29 12:07:21 +0200

    modify index.php, add index.scc and add backgroudImage

--- src/index.php
+++ src/index.php
@@ -5,13 +5,21 @@ session_start()
 <html lang="en">
 <head>
     <?php require('./includes/head.php') ?>
+    <link rel="stylesheet" href="/public/style/index.css">
     <title>MyDigitalSucre</title>
 </head>
 <body>
     <?php require('./includes/nav.php') ?>
-    <h1>Accueil</h1>
-    <?= $_SESSION && $_SESSION['user'] ?  $_SESSION['user']['email'] : 'guest' ?>
+    <div id="backgroundImage">
+    <h1>Bienvenue <?= $_SESSION && $_SESSION['user'] ?  $_SESSION['user']['email'] : 'guest' ?></h1>
+    
     <br>
-    <?= $_SESSION['user']['credit'] ?>
+    <h3>Envoyez un message sucré à votre ami(e)</h3>
+   <div class="buttons">
+    <button>Connexion</button> <button>S'inscrire</button>
+
+   </div>
+    </div>
+    
 </body>
 </html>
