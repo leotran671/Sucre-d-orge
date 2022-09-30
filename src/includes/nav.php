ff443c538f331f9ad03b451a0cc02ae6afc17685
Hugo <mollow@Mollow.local>
2022-09-30 10:20:06 +0200

    Fix Among

--- src/includes/nav.php
+++ src/includes/nav.php
@@ -15,7 +15,8 @@
          <li><a <?= $_SERVER['REQUEST_URI'] == "/signin.php" ? "aria-disabled=true" : "href=/signin.php" ?> >Inscription</a></li>
       <?php endif ?>
    </ul>
-   <a href="login.php"><img src="public/medias/AmongUs.png" id="AmongUs" width="40" height="40" alt="Among Us"></a>
-   
-</nav>
+   <a href="login.php" class="Egg">
+      <img src="public/medias/AmongUs.png" width="40" height="40" alt="Among Us">
+   </a>
+   </nav>
 <script src="./public/js/amongUs.js"></script>
